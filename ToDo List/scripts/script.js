99a4fcc742df3cc53dc102bc11b9b790abf6edd3
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-05-08 11:17:10 +0300

    remove

--- ToDo List/scripts/script.js
+++ ToDo List/scripts/script.js
@@ -1,68 +1,102 @@
-'use strict';
+ 'use strict';
 
 const todoControl = document.querySelector('.todo-control'),
-    headerInput = document.querySelector('.header-input'),
-    todoList = document.querySelector('.todo-list'),
-    todoCompleted = document.querySelector('.todo-completed');
+      headerInput = document.querySelector('.header-input'),
+      todoList = document.querySelector('.todo-list'),
+      todoCompleted = document.querySelector('.todo-completed');
 
-const todoData = [];
+const todoData = [
+  
+    ];
 
-const render = function() {
- todoList.textContent = '';
-todoCompleted.textContent = ''; 
+    const render = function() {
 
-  todoData.forEach(function (item) {
-    const li = document.createElement('li');
-    li.classList.add('todo-item');
-    li.innerHTML = '<span class="text-todo"> '+ item.value + '</span>' +
-        '<div class="todo-buttons">' +
+      todoList.textContent = '';
+      todoCompleted.textContent = '';
+
+      todoData.forEach(function(item, index){
+        //console.log(item)
+        let li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo"> '+ item.value + '</span>' +
+         '<div class="todo-buttons">' +
             '<button class="todo-remove"></button>' +
             '<button class="todo-complete"></button>' +
-        '</div>';
-         if(item.completed) {
-           todoCompleted.append(li);
-         } else {
-           todoList.append(li);
-         }
-         const btnTodoCompleted = li.querySelector('.todo-complete');
-         const btnTodoRemove = li.querySelector('.todo-remove');
-         
-         btnTodoCompleted.addEventListener('click', function () {
-           item.completed = !item.completed;
-           render();
-         });
-         btnTodoRemove.addEventListener('click', function () {
-             li.remove()
-           //render();
-         });       
-  });
-};
- const showTodo = function() {
-    todoList.textContent = localStorage.getItem('list');
-    // todoList.textContent = JSON.parse(todoData);
+            '</div>';
+        //li.dataValue = item.value
+        li.dataIndex = index//todoData.indexOf(item)
+
+        if(item.completed) {
+             todoCompleted.append(li);
+        } else {
+          todoList.append(li); 
+        }
+
+        const btnTodoCompleted = li.querySelector('.todo-complete');
+        btnTodoCompleted.addEventListener('click', function () {
+          item.completed = !item.completed;
+          render();   
+        });
+        const btnTodoRemove = li.querySelector('.todo-remove');
+        btnTodoRemove.addEventListener('click', function(){
+           //li.style.display = 'none';
+           
+           //console.log(li.dataValue)
+           /*
+           todoData.forEach(function(item){
+            if (item.value == li.dataValue){
+              let index = todoData.indexOf(item);
+              if (index > -1) {
+                todoData.splice(index, 1);
+              }
+            }
+           })
+           */
+           todoData.splice(li.dataIndex,1)
+           render()
+        });  
+
+      }); 
+    };
+    todoControl.addEventListener('submit', function(event){
+      event.preventDefault()
+      const newTodo = {
+        value: headerInput.value,
+        completed: false
+      };
+      if (newTodo.value !==''){
+        todoData.push(newTodo);   
+      };
+      headerInput.value = '';
+      render();
+       let todo = JSON.stringify(todoData);
+      localStorage.setItem('list', todo);
+      
+    });
+    render();
+
+// let arr = ["Bilbo", "Gandalf", "Nazgul"];
+// console.log(arr);
+/*arr.forEach((item, index, array) => {
+   // console.log(`${item} имеет позицию ${index} в ${array}`);
+   console.log(item, index)
+  if(item === "Gandalf"){
+  arr.splice(index, 1); 
   }
-  showTodo();
+  if(item === "Nazgul"){
+  arr.splice(index, 1); 
+  }
 
-todoControl.addEventListener('submit', function(event){
-  event.preventDefault();
-
-const newTodo = {
-  value: headerInput.value,
-  completed: false
-}
-if (newTodo.value !==''){
-  todoData.push(newTodo);
-   
-}
-
-  headerInput.value = '';
-
-  let json = JSON.stringify(todoData);
-
-  localStorage.setItem('list', json);
-  showTodo();
-
-  render();
+  
 });
-render();
+*/
+// function isBigEnough(value) {
+//   return value[0] != "G" && value[0] != "N";
+// }
+
+// let filtered = arr.filter(isBigEnough);
+// // массив filtered равен [12, 130, 44]
+// console.log(filtered);
+
 
