67c9c9f72d6559d57e217fc3fece2b6e413f845f
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-05-09 16:33:43 +0300

    setting time

--- dayOfRestTask/js/script.js
+++ dayOfRestTask/js/script.js
@@ -9,7 +9,11 @@ let userR = [];
 
 let date = new Date;
 
+console.log(date.getFullYear());
+const monthes = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
+const month = monthes[date.getMonth()];
+console.log(month);
 const userList = function(){
   list.textContent = '';
 
@@ -17,16 +21,12 @@ const userList = function(){
     let li = document.createElement('li');
       li.classList.add('user-item');
 
-      li.textContent = item[0] + ' ' + item[1] + ' ' + date;
-
+   li.textContent = 'имя: ' + item[0] + ' фамилия:  ' + item[1] + ' дата регистрации:  ' + date.getDate() + ' ' + month  + ' ' + date.getFullYear() + ' года ' + ' в ' + date.getHours() + ':' + date.getMinutes()+';';
       list.append(li);
  });
 
    
 };
-
-
-
 
 registerUser.addEventListener('click', () =>{
 const	init = prompt("Your first name and name"),
